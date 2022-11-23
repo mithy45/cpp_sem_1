e8bfc5ef92200bef706dca70013e446f18f5d543
quentin1.guyot <quentin1.guyot@epita.fr>
2022-11-23 21:35:06 +0100

    selection sort

---
+++ selection_sort/my_sort.hpp
@@ -0,0 +1,26 @@
+// Change me instead
+// All occurences of XXX need to be replaced with
+// something more meaningful
+
+template<class TYPE>
+void swap(TYPE& a, TYPE& b) {
+    TYPE tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template<class IT>
+void my_selection_sort( IT begin, IT end)
+{
+    for (IT i = begin; i != end; ++i)
+    {
+        IT min = i;
+        IT next = i; 
+        for (IT j = ++next; j != end; ++j)
+        {
+            if (*j < *min)
+                min = j;
+        }
+        swap(*min, *i);
+    }
+}
